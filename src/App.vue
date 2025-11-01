2ccd3eaf226233cec12a0b858873fb0b1159f1db
quockiet0505 <dqkiet051104@gmail.com>
2025-11-02 01:14:14 +0700

    Hien thi danh sach contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <script>
-  export default{
-
-  }
+import AppHeader from '@/components/AppHeader.vue';
+export default {
+  components: {
+    AppHeader,
+  },
+};
 </script>
 
 <template>
-  <h1>Hello, Vue.js</h1>
+  <div id="app">
+    <AppHeader />
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <style>
-  .page{
-    max-width: 400px;
-    margin: auto;
-  }
+
+#app {
+  display: flex;
+  flex-direction: column; 
+  min-height: 100vh; 
+}
+
+/* giữ style cũ */
+.page {
+  max-width: 400px;
+  margin: auto;
+}
 </style>
---
+++ src/components/InputSearch.vue
@@ -0,0 +1,33 @@
+<script>
+     export default{
+          props:{
+               modelValue: {type: String, default: ''},
+          },
+          emits: ["submit", "update:modelValue"],
+          methods:{
+               updateModelValue(event){
+                    this.$emit("update:modelValue", event.target.value);
+               },
+               submit(){
+                    this.$emit("submit");
+               }
+          }
+     }
+</script>
+<template>
+     <div class="input-group">
+          <input 
+               type="text" 
+               class="form-control" 
+               :value="modelValue" 
+               @input="updateModelValue"
+               @keyup.enter="submit"
+               placeholder="Nhập thông tin cần tìm"
+          >
+          <div class="input-group-append">
+               <button class="btn-outline-secondary btn" @click="submit">
+                   <i class="fas fa-search"></i>Tìm kiếm
+               </button>
+          </div>
+     </div>
+</template>
